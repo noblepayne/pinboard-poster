ca73f8801e535595e3aba794e5fe25b23703f02e
Wes Payne <noblepayne@noblepayne.com>
2026-06-03 14:52:53 -0700

    feat: explicit feed→channel configuration

--- src/noblepayne/pinboard_poster.clj
+++ src/noblepayne/pinboard_poster.clj
@@ -3,10 +3,8 @@
                        :linters {:unresolved-symbol {:exclude [(datalevin.core/with-conn)]}}}}
   (:gen-class)
   (:require [babashka.fs :as fs]
-            [clojure.data.xml :as xml]
             [clojure.string :as str]
             [clojure.zip :as zip]
-            [cheshire.core :as json]
             [datalevin.core :as d]
             [hato.client :as http]
             [hickory.core :as h]
@@ -14,7 +12,16 @@
             [hickory.zip :as hz]))
 
 ;; ~~~~~~~~~~ Global Config ~~~~~~~~~~
-(def FEEDS ["https://feeds.pinboard.in/rss/u:noblepayne/t:lup"])
+(def FEED-CONFIG
+  [{:feed "https://feeds.pinboard.in/rss/u:noblepayne/t:lup"
+    :channel "#lup-links"
+    :label "LUP Pinboard"}
+   {:feed "https://feeds.pinboard.in/rss/u:tophfisher/t:clanker"
+    :channel "#clanker-links"
+    :label "Clanker Pinboard"}
+   {:feed "https://feeds.pinboard.in/rss/u:noblepayne/t:clanker"
+    :channel "#clanker-links"
+    :label "Clanker Pinboard"}])
 (def SLACK-URL (System/getenv "SLACK_URL"))
 (def SCHEMA {;; feed url
              :feed/id {:db/valueType :db.type/string}
@@ -32,12 +39,11 @@
 (def KV-OPTS {:kv-opts {:mapsize 2}})
 
 ;; ~~~~~~~~~~ Feed Processing ~~~~~~~~~~
+(defn parse-feed [xml-string]
+  (-> xml-string h/parse h/as-hickory))
+
 (defn pull-feed [feed-url]
-  (-> feed-url
-      http/get
-      :body
-      h/parse
-      h/as-hickory))
+  (-> feed-url http/get :body parse-feed))
 
 (defn find-items [feed]
   (hs/select (hs/tag :item) feed))
@@ -70,7 +76,8 @@
 ;; ~~~~~~~~~~ Database Storage ~~~~~~~~~~
 (defn store-item [conn feed-id item-id]
   (d/transact! conn [{:feed/id feed-id
-                      :feed.item/id item-id}]))
+                      :feed.item/id item-id
+                      :feed.item/unique-id [feed-id item-id]}]))
 
 (defn all-entities [conn]
   (d/q '[:find (d/pull ?e ["*"])
@@ -78,75 +85,72 @@
        (d/db conn)))
 
 (defn find-item [conn feed-url item-id]
-  (first (d/q '[:find (d/pull ?e ["*"])
-                :in $ ?feed-id ?item-id
-                :where
-                [?e :feed/id ?feed-id]
-                [?e :feed.item/id ?item-id]]
-              (d/db conn)
-              feed-url
-              item-id)))
+  (ffirst (d/q '[:find (d/pull ?e ["*"])
+                 :in $ ?feed-id ?item-id
+                 :where
+                 [?e :feed/id ?feed-id]
+                 [?e :feed.item/id ?item-id]]
+               (d/db conn)
+               feed-url
+               item-id)))
 
 (defn retract-all [conn e]
   (d/transact! conn [[:db/retractEntity e]]))
 
-(defn unseen-items [conn item-ids]
+(defn unseen-items [conn feed-id item-ids]
   (let [results (d/q '[:find ?id
-                       :in $ [?id ...]
-                       :where (not [_ :feed.item/id ?id])]
+                       :in $ ?feed-id [?id ...]
+                       :where (not [?e :feed/id ?feed-id]
+                                   [?e :feed.item/id ?id])]
                      (d/db conn)
+                     feed-id
                      item-ids)]
     (into [] (map first) results)))
 
 ;; ~~~~~~~~~~ Slack ~~~~~~~~~~
-(defn format-message [{:keys [author link title description]}]
-  (str "_" author "'s LUP Pinboard:_\n"
+(defn format-message [{:keys [author link title description]} label]
+  (str "_" author "'s " label ":_\n"
        "<" link "|" title ">"
        (when description (str "\n" description))))
 
-(defn post-to-slack [feed-item]
-  (->  SLACK-URL
-       (http/post
-        {:content-type :json
-         :throw-exceptions? false
-         :form-params {:channel "#lup-links",
-                       :username "kirkland_brand_zapier",
-                       :text (format-message feed-item)
-                       :unfurl_links true}})
-       :satus))
+(defn post-to-slack [feed-item channel label]
+  (-> SLACK-URL
+      (http/post
+       {:content-type :json
+        :throw-exceptions? false
+        :form-params {:channel channel
+                      :username "kirkland_brand_zapier"
+                      :text (format-message feed-item label)
+                      :unfurl_links true}})
+      :status))
 
 ;; ~~~~~~~~~~ Main ~~~~~~~~~~
-(defn process-feed [conn feed-url]
-  (let [feed (pull-feed feed-url)
-        items (find-items feed)
+(defn process-feed-config [conn {:keys [feed channel label]}]
+  (let [items (-> feed pull-feed find-items)
         parsed-items (map (comp (juxt :id identity) extract-feed-item) items)
         item-map (apply array-map (flatten parsed-items))
         all-ids (keys item-map)
-        ;; This could get stale but that's ok in this use case.
-        ;; We have uniqueness tracking in the db if something new has been added
-        ;; to the db. If something is retracted we won't be including it, but a
-        ;; subsequent run will catch it.
-        unseen-ids (unseen-items conn all-ids)]
+        unseen-ids (unseen-items conn feed all-ids)]
     (doseq [id unseen-ids
             :let [item (get item-map id)]]
-      (d/with-transaction [txn conn]
-        (try
-          (store-item txn feed-url id)
-          (post-to-slack item)
-          (println "Processed:" id)
-          (catch clojure.lang.ExceptionInfo e (when (not= :transact/unique
-                                                          (-> e ex-data :error))
-                                                (throw e)))
-          (catch Exception e (d/abort-transact txn) (throw e)))))))
+      (let [status (post-to-slack item channel label)]
+        (when (and status (<= 200 status 299))
+          (d/with-transaction [txn conn]
+            (try
+              (store-item txn feed id)
+              (println "Processed:" id)
+              (catch clojure.lang.ExceptionInfo e
+                (when (not= :transact/unique (-> e ex-data :error))
+                  (throw e))))))))))
 
 (defn -main [& _]
   (d/with-conn [conn "db" SCHEMA KV-OPTS]
-    (doseq [feed FEEDS]
-      (process-feed conn feed))
+    (doseq [fc FEED-CONFIG]
+      (process-feed-config conn fc))
     ;; re-init db to reduce size 
     ;; BUG? second opening of db always resizes to 100M
     ;; re-init respects KV-OPTS and tries to fit it into mapsize.
-    (d/init-db (d/datoms (d/db conn) :eav) "db2" SCHEMA KV-OPTS)) 
+    (d/init-db (d/datoms (d/db conn) :eav) "db2" SCHEMA KV-OPTS))
   ;; Replace db with fresh version.
   ;; TODO: replace with d/copy?
   (fs/delete-tree "db")
@@ -157,5 +161,5 @@
   (-main)
 
   (def conn (d/get-conn "db" SCHEMA KV-OPTS))
-  (process-feed conn (first FEEDS))
+  (process-feed-config conn (first FEED-CONFIG))
   (d/close conn))
